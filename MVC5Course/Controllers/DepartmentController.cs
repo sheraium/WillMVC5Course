57dc66e248ce1567d056e2a0c14a902d90ea1e20
320 陳永修 <peterch@mirle.com.tw>
2019-05-11 16:34:53 +0800

    練習透過 ViewModel 快速建立表單與 Actions

---
+++ MVC5Course/Controllers/DepartmentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(DepartmentCreationVM data)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+    }
+}

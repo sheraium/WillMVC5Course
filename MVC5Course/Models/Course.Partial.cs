cba156ec01306cfb077c4ea4f4701905c87026d2
320 陳永修 <peterch@mirle.com.tw>
2019-05-12 11:13:11 +0800

    練習擴充資料模型與自訂額外的驗證屬性

---
+++ MVC5Course/Models/Course.Partial.cs
@@ -0,0 +1,32 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(CourseMetaData))]
+    public partial class Course
+    {
+    }
+    
+    public partial class CourseMetaData
+    {
+        [Required]
+        public int CourseID { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string Title { get; set; }
+        [Required]
+        [Range(1,5, ErrorMessage = "1~5")]
+        public int Credits { get; set; }
+        [Required]
+        public int DepartmentID { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string Location { get; set; }
+    
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Person> People { get; set; }
+    }
+}

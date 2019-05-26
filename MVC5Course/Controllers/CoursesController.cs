181a5ac73dd2534d7a7e5d06d290c5f9a226fc34
320 陳永修 <peterch@mirle.com.tw>
2019-05-26 14:49:07 +0800

    示範 [ChildActionOnly] 用法

--- MVC5Course/Controllers/CoursesController.cs
+++ MVC5Course/Controllers/CoursesController.cs
@@ -30,6 +30,7 @@ namespace MVC5Course.Controllers
 
         // GET: Courses
         [Route("list/{deptId}")]
+        [ChildActionOnly]
         public ActionResult List(int deptId)
         {
             var course = courseRepo.All().Where(p => p.DepartmentID == deptId);
